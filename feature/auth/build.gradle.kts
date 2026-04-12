bbcb566541fbe2fc2add4c090b7bd631443b38dd
ayushkeshri19 <keshri.ayush019@gmail.com>
2026-04-13 02:28:30 +0530

    clean local db on user log out

--- feature/auth/build.gradle.kts
+++ feature/auth/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
 
     implementation(project(":core:common"))
     implementation(project(":core:network"))
+    implementation(project(":core:database"))
     implementation(project(":core:datastore"))
     implementation(project(":core:ui"))
 
--- feature/auth/src/main/java/com/ayush/auth/domain/usecase/SignOutUseCase.kt
+++ feature/auth/src/main/java/com/ayush/auth/domain/usecase/SignOutUseCase.kt
@@ -1,14 +1,22 @@
 package com.ayush.auth.domain.usecase
 
 import com.ayush.auth.data.repository.AuthRepository
+import com.ayush.database.LedgeDatabase
+import com.ayush.datastore.domain.repository.AppDataStoreRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class SignOutUseCase @Inject constructor(
-    private val authRepository: AuthRepository
+    private val authRepository: AuthRepository,
+    private val database: LedgeDatabase,
+    private val dataStore: AppDataStoreRepository
 ) {
     suspend operator fun invoke() {
-        withContext(Dispatchers.IO) { authRepository.signOut() }
+        withContext(Dispatchers.IO) {
+            database.clearAllTables()
+            dataStore.clearData()
+            authRepository.signOut()
+        }
     }
 }
