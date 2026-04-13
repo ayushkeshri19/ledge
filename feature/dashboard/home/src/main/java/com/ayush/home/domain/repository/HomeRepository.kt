eecdeeae10e7b129c4b1b428a209bfc17e872fc0
ayushkeshri19 <keshri.ayush019@gmail.com>
2026-04-13 14:59:23 +0530

    set up repository layer

--- feature/dashboard/home/src/main/java/com/ayush/home/domain/repository/HomeRepository.kt
+++ feature/dashboard/home/src/main/java/com/ayush/home/domain/repository/HomeRepository.kt
@@ -1,7 +1,13 @@
 package com.ayush.home.domain.repository
 
 import com.ayush.common.models.User
+import com.ayush.database.data.CategorySpendTuple
+import com.ayush.database.data.TransactionWithCategory
 
 interface HomeRepository {
     suspend fun getCurrentUser(): User?
+    suspend fun getTotalIncome(startDate: Long, endDate: Long): Double
+    suspend fun getTotalExpense(startDate: Long, endDate: Long): Double
+    suspend fun getExpensesByCategory(startDate: Long, endDate: Long): List<CategorySpendTuple>
+    suspend fun getRecentTransactions(limit: Int): List<TransactionWithCategory>
 }
