1ada67c379bb532ff55ebdd44dbd57fd87164deb
ayushkeshri19 <keshri.ayush019@gmail.com>
2026-04-26 00:10:58 +0530

    new worker tag

--- core/common/src/main/java/com/ayush/common/utils/Workers.kt
+++ core/common/src/main/java/com/ayush/common/utils/Workers.kt
@@ -3,4 +3,5 @@ package com.ayush.common.utils
 object Workers {
     const val TRANSACTION_SYNC: String = "transaction_sync"
     const val RECURRING_TRANSACTION: String = "recurring_transaction"
+    const val RECURRING_TRANSACTION_IMMEDIATE: String = "recurring_transaction_immediate"
 }
